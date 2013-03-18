b0ff5caceb66448a01bda606d27cc638da104f89
kris <kris@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-18 08:31:16 +0000

    Sam/Kris - #55 - fixing startup script again
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@46 363a94d9-e023-4002-b4ac-95dfb99b3300

--- scripts/startDropwizard.sh
+++ scripts/startDropwizard.sh
@@ -2,5 +2,5 @@
 
 PIDFILE=/var/run/bloottorrent.pid
 
-java -jar ../target/bloodtorrent-1.0.0-SNAPSHOT.jar server ../src/main/resources/configurations.yml &
+java -jar target/bloodtorrent-1.0.0-SNAPSHOT.jar server src/main/resources/configurations.yml &
 echo $! > $PIDFILE
