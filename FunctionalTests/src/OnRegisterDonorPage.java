3de72f4aa5fdbd6d8b6a7064013f7a0a2f4ebf7d
jaemin <jaemin@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-20 05:18:01 +0000

    [Lindsay] remove error(it's not completed)
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@85 363a94d9-e023-4002-b4ac-95dfb99b3300

---
+++ FunctionalTests/src/OnRegisterDonorPage.java
@@ -0,0 +1,38 @@
+// JUnit Assert framework can be used for verification
+
+import net.sf.sahi.client.Browser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.thoughtworks.twist.core.execution.TwistScenarioDataStore;
+
+public class OnRegisterDonorPage {
+
+	private Browser browser;
+
+	@Autowired
+	private TwistScenarioDataStore scenarioStore;
+
+	public OnRegisterDonorPage(Browser browser) {
+		this.browser = browser;
+	}
+
+	public void fillOutTheValidInformationToMandatoryFields() throws Exception {
+		browser.textbox("firstName").setValue("Jane");
+		browser.textbox("lastName").setValue("Jung");
+		browser.textbox("email").setValue("Jane@samsung.com");
+		browser.password("password").setValue("12341234");
+		browser.password("confirmPassword").setValue("12341234");
+		browser.textarea("address").setValue("Seoul");
+		browser.textbox("city").setValue("Seoul");
+		browser.select("state").choose("Orissa");
+		browser.textbox("cellPhone").setValue("0102003000");
+		browser.select("bloodGroup").choose("B+");
+		browser.select("distance").choose("10");
+	}
+
+	public void registerDonor(String name) throws Exception {
+		browser.button(name).click();
+	}
+
+}
