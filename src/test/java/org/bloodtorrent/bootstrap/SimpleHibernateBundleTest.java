d2397c3d272d00875f921095e1b714ca556e8244
sam <sam@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-20 03:04:11 +0000

    [Sam/Inchul/Scott] #74 - Modify incorrect test case
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@77 363a94d9-e023-4002-b4ac-95dfb99b3300

--- src/test/java/org/bloodtorrent/bootstrap/SimpleHibernateBundleTest.java
+++ src/test/java/org/bloodtorrent/bootstrap/SimpleHibernateBundleTest.java
@@ -1,19 +1,12 @@
 package org.bloodtorrent.bootstrap;
 
-import com.google.common.collect.Iterables;
-import org.bloodtorrent.dto.BloodReq;
+import org.bloodtorrent.dto.BloodRequest;
 import org.bloodtorrent.dto.Person;
 import org.bloodtorrent.dto.User;
-import org.hamcrest.collection.IsArrayContaining;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.hamcrest.Matcher.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 
@@ -31,7 +24,7 @@ public class SimpleHibernateBundleTest {
     public void shouldGatherEntitiesWithinPackage(){
         bundle = new SimpleHibernateBundle("org.bloodtorrent");
         assertTrue(Arrays.asList(bundle.getEntityClasses()).contains(User.class));
-        assertTrue(Arrays.asList(bundle.getEntityClasses()).contains(BloodReq.class));
+        assertTrue(Arrays.asList(bundle.getEntityClasses()).contains(BloodRequest.class));
         assertTrue(Arrays.asList(bundle.getEntityClasses()).contains(Person.class));
     }
 }
