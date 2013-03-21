4a8223d389d5ca53340bcd6015f7ebbc63bba54d
kris <kris@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-21 04:22:04 +0000

    KY/KS - modifiy bloodtorrent.iml and add UserViewTest
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@109 363a94d9-e023-4002-b4ac-95dfb99b3300

--- src/test/java/view/UserViewTest.java
+++ src/test/java/view/UserViewTest.java
@@ -1,5 +1,12 @@
 package view;
 
+import org.bloodtorrent.view.UserView;
+import org.junit.Test;
+import org.bloodtorrent.dto.User;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created with IntelliJ IDEA.
  * User: sds
@@ -8,4 +15,12 @@ package view;
  * To change this template use File | Settings | File Templates.
  */
 public class UserViewTest {
+
+    @Test
+    public void shouldGetUser(){
+        User user = new User();
+        UserView userView = new UserView(user);
+
+        assertThat(userView.getuser(), is(user));
+    }
 }
