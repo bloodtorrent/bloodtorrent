70c9ce97ed984bf4c31df5fffc9d42cd7bd22bf4
kris <kris@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-21 04:21:31 +0000

    KY/KS - add Testcase for UserView
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@108 363a94d9-e023-4002-b4ac-95dfb99b3300

---
+++ src/test/java/view/UserViewTest.java
@@ -0,0 +1,11 @@
+package view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sds
+ * Date: 13. 3. 21
+ * Time: 오후 1:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserViewTest {
+}
