8cbeb692f2d0641d09fa30b75efaa272815b789d
sam <sam@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-29 01:03:00 +0000

    Kris/Scott/DongHo - modifying build script to execute remote dropwizard start with maven & changing pom to antrun the jar file
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@263 363a94d9-e023-4002-b4ac-95dfb99b3300

--- scripts/remote-deploy-and-start.sh
+++ scripts/remote-deploy-and-start.sh
@@ -1,4 +1,4 @@
 cd bloodtorrent-web
 sh ./scripts/stopDropwizard.sh
 mvn dbdeploy:update
-nohup java -jar target/bloodtorrent-1.0.0-SNAPSHOT.jar server src/main/resources/configurations.yml &
+mvn antrun:run
