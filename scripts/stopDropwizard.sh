7df2c4afce9d3b987d519c6f96cb145e8a650deb
saleem <saleem@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-03-29 07:20:43 +0000

    [saleem] Added -E switch to grep (needed for regex) in stopDropWizard.sh
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@281 363a94d9-e023-4002-b4ac-95dfb99b3300

--- scripts/stopDropwizard.sh
+++ scripts/stopDropwizard.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PID=`jps -v | grep bloodtorrent-.*-SNAPSHOT.jar | awk '{print $1}'`
+PID=`jps -v | grep -E bloodtorrent-.*-SNAPSHOT.jar | awk '{print $1}'`
 
 if [ ! -z "$PID" ] ; then
     kill -9 $PID
