6525d01f98d3a1542647c78dc8a69a708557ee73
james <james@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-04-01 07:37:40 +0000

    [bonna, saleem] Added code to the stopDropwizard.sh script so that it works on both Linux and non-Linux (=Windows) OSes
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@308 363a94d9-e023-4002-b4ac-95dfb99b3300

--- scripts/stopDropwizard.sh
+++ scripts/stopDropwizard.sh
@@ -3,5 +3,11 @@
 PID=`jps -v | grep -E bloodtorrent-.*-SNAPSHOT.jar | awk '{print $1}'`
 
 if [ ! -z "$PID" ] ; then
+  if [ `uname` = 'Linux' ] ; then
+    echo "Linux detected, using kill -9 to kill jps process"
     kill -9 $PID
+  else
+    echo "Assuming windows, using taskkill to kill jps process"
+    taskkill -f -pid $PID
+  fi
 fi
