d25ec6a711bd65659790275652f0ab0c5074c054
james <james@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-04-04 00:49:05 +0000

    [James/Inchul] #79 modified about messages
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@368 363a94d9-e023-4002-b4ac-95dfb99b3300

--- src/main/resources/js/successStories.js
+++ src/main/resources/js/successStories.js
@@ -9,7 +9,7 @@ var suppressSubmitAndNotifyIfInvalid = function() {
         alert('You can choose '+maxChecks+' success stories at most');
         return false;
     } else if (numChecked == 0){
-        alert('Please, select 1 ~ 3 stories for displaying at main page');
+        alert('Please, select 1 ~ 3 stories for displaying on the main page');
         return false;
     }
     return true;
