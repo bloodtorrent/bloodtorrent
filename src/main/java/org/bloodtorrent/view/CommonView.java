73053772dfee9f2c04b1ffccfd8c6457917ed561
james <james@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-04-08 02:43:02 +0000

    Sam/James - changed separator to '/' in creating ResourceBundle
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@416 363a94d9-e023-4002-b4ac-95dfb99b3300

--- src/main/java/org/bloodtorrent/view/CommonView.java
+++ src/main/java/org/bloodtorrent/view/CommonView.java
@@ -32,23 +32,15 @@ public class CommonView extends View {
 
     public CommonView(String viewPath) {
         super(viewPath);
-        printClassPath("String constructor");
-    }
-
-    private void printClassPath(String whichConstructor) {
-        String classPath = System.getProperty("java.class.path");
-        System.out.printf("Using %s constructor. Classpath is [%s]\n", whichConstructor, classPath);
     }
 
     public CommonView(String viewPath, User user){
         super(viewPath);
         this.user = user;
-        printClassPath("String, User constructor");
     }
 
     public CommonView(String viewPath, String message) {
         super(viewPath);
         this.message = message;
-        printClassPath("String, String constructor");
     }
 }
