1b2c411086231204511231491289922ca1fca9d2
kris <kris@363a94d9-e023-4002-b4ac-95dfb99b3300>
2013-04-08 02:23:52 +0000

    [saleem] Adding UNTESTED exploratory code to print classpath; figuring out the broken app problem
    git-svn-id: svn://192.168.0.100/project/trunk/bloodtorrent@414 363a94d9-e023-4002-b4ac-95dfb99b3300

--- src/main/java/org/bloodtorrent/view/CommonView.java
+++ src/main/java/org/bloodtorrent/view/CommonView.java
@@ -23,6 +23,7 @@ public class CommonView extends View {
     @Getter @Setter
     private String message;
 
+
     @Getter
     private ResourceBundle printStrings = ResourceBundle.getBundle("properties" + separator + "bloodtorrentStrings");
 
@@ -31,15 +32,23 @@ public class CommonView extends View {
 
     public CommonView(String viewPath) {
         super(viewPath);
+        printClassPath("String constructor");
+    }
+
+    private void printClassPath(String whichConstructor) {
+        String classPath = System.getProperty("java.class.path");
+        System.out.printf("Using %s constructor. Classpath is [%s]\n", whichConstructor, classPath);
     }
 
     public CommonView(String viewPath, User user){
         super(viewPath);
         this.user = user;
+        printClassPath("String, User constructor");
     }
 
     public CommonView(String viewPath, String message) {
         super(viewPath);
         this.message = message;
+        printClassPath("String, String constructor");
     }
 }
